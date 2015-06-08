6a523fc6f486ffa021cb5293bd138eb2e07e80c1
Romanx <mcauliffe.alex@gmail.com>
2015-06-08 20:33:35 +0100

    Root class for Rendering

---
+++ Stubble/Stubble.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stubble.Core
+{
+    public class Stubble
+    {
+        public static string Render(string template, object view)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
