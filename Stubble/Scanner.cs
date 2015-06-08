61c42ac125250c043a41a87bbfba3ec4b1549200
Romanx <mcauliffe.alex@gmail.com>
2015-06-08 20:43:31 +0100

    Took Scanner written for Nustache by Me

---
+++ Stubble/Scanner.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Stubble.Core
+{
+    public class Scanner
+    {
+        public string Template { get; set; }
+        public string Tail { get; set; }
+        public int Pos { get; set; }
+        public bool EOS { get { return string.IsNullOrEmpty(Tail); } }
+
+        public Scanner(string template)
+        {
+            Template = template;
+            Tail = template;
+            Pos = 0;
+        }
+
+        public string Scan(Regex expression)
+        {
+            var matched = expression.Match(Tail);
+
+            if (!matched.Success || matched.Index != 0)
+            {
+                return "";
+            }
+
+            var result = matched.Value;
+            Tail = Tail.Substring(result.Length);
+            Pos += result.Length;
+
+            return result;
+        }
+
+        public string Scan(string expression)
+        {
+            return Scan(new Regex(expression));
+        }
+
+        public string ScanUntil(Regex expression)
+        {
+            var regexMatch = expression.Match(Tail);
+            var index = regexMatch.Index;
+            string match;
+
+            if (!regexMatch.Success)
+            {
+                match = Tail;
+                Tail = "";
+            }
+            else if (index == 0)
+            {
+                match = "";
+            }
+            else
+            {
+                match = Tail.Substring(0, index);
+                Tail = Tail.Substring(index);
+            }
+
+            Pos += match.Length;
+            return match;
+        }
+
+        public string ScanUntil(string expression)
+        {
+            return ScanUntil(new Regex(expression));
+        }
+    }
+}
