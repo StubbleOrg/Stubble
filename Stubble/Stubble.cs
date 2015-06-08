d0d000c72fda7e5fc4c0777e0e226fc7342128fb
Romanx <mcauliffe.alex@gmail.com>
2015-06-08 21:51:42 +0100

    Parse method on Stubble class

--- Stubble/Stubble.cs
+++ Stubble/Stubble.cs
@@ -1,16 +1,30 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Stubble.Core.Classes;
 
 namespace Stubble.Core
 {
     public class Stubble
     {
-        public static string Render(string template, object view)
+        private ConcurrentDictionary<string, IEnumerable<ParserOutput>> Cache { get; set; }
+
+        public string Render(string template, object view)
         {
             throw new NotImplementedException();
+        }
+
+        public IEnumerable<ParserOutput> Parse(string template, Tags tags)
+        {
+            IEnumerable<ParserOutput> tokens;
+            var success = Cache.TryGetValue(template, out tokens);
+            if (!success)
+                tokens = Cache[template] = Parser.ParseTemplate(template, tags);
+
+            return tokens;
         }
     }
 }
