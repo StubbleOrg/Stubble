80df9fcf66b70943f530a037e8b1773d257e08f1
Romanx <mcauliffe.alex@gmail.com>
2015-07-03 22:00:26 +0100

    Added nuspec for publishing

--- Stubble/Properties/AssemblyInfo.cs
+++ Stubble/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Stubble")]
-[assembly: AssemblyDescription("Trimmed Down Mustache Templating for .Net")]
+[assembly: AssemblyDescription("Trimmed down {{mustache}} templates in .NET")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Alex McAuliffe")]
 [assembly: AssemblyProduct("Stubble")]
