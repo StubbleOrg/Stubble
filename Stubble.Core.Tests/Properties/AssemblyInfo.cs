780e726620e3a3044870e936154cff25c5597847
Romanx <mcauliffe.alex@gmail.com>
2015-07-03 20:44:57 +0100

    Signed both the Core and Test repos

--- Stubble.Core.Tests/Properties/AssemblyInfo.cs
+++ Stubble.Core.Tests/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Stubble.Core.Tests")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Tests for Stubble.Core")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Alex McAuliffe")]
 [assembly: AssemblyProduct("Stubble.Core.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.1")]
-[assembly: AssemblyFileVersion("0.0.0.1")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
--- Stubble/Properties/AssemblyInfo.cs
+++ Stubble/Properties/AssemblyInfo.cs
@@ -6,14 +6,19 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Stubble")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Trimmed Down Mustache Templating for .Net")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Alex McAuliffe")]
 [assembly: AssemblyProduct("Stubble")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: InternalsVisibleTo("Stubble.Core.Tests")]
+[assembly: InternalsVisibleTo("Stubble.Core.Tests,PublicKey=" +
+"002400000480000094000000060200000024000052534131000400000100010019009f832e2170" +
+"5580ec3a2b12212159825355989853ceb857774e50f4d066572e17a55f050c06c91e458cd1c19a" +
+"62545fce5eeae15ac65ab9a734997f9c60632b13cc663c4e318ac01de16a2d761ae2eedbf04bc5" +
+"792ae156e6a698197bb3b7e0270fc43fd48a7e8925344ac92ef314aa91b67f55f38b74a27096da" +
+"a723e1d3")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -33,5 +38,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
