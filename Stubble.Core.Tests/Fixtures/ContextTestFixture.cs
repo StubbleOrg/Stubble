c140ae001d8aa235ca09e44d5871c529f7491357
Romanx <mcauliffe.alex@gmail.com>
2015-07-05 21:21:34 +0100

    Removed some deadcode from tests

--- Stubble.Core.Tests/Fixtures/ContextTestFixture.cs
+++ Stubble.Core.Tests/Fixtures/ContextTestFixture.cs
@@ -13,38 +13,6 @@ namespace Stubble.Core.Tests.Fixtures
     {
         public Context Context { get; set; }
 
-        public IDictionary<Type, Func<object, string, object>> Registry =
-            new ReadOnlyDictionary<Type, Func<object, string, object>>
-        (
-            new Dictionary<Type, Func<object, string, object>>
-            {
-                {
-                    typeof (IDictionary<string, object>),
-                    (value, key) =>
-                    {
-                        var castValue = value as IDictionary<string, object>;
-                        return castValue != null ? castValue[key] : null;
-                    }
-                },
-                {
-                    typeof (IDictionary),
-                    (value, key) =>
-                    {
-                        var castValue = value as IDictionary;
-                        return castValue != null ? castValue[key] : null;
-                    }
-                },
-                {
-                    typeof (object), (value, key) =>
-                    {
-                        var type = value.GetType();
-                        var propertyInfo = type.GetProperty(key);
-                        return propertyInfo != null ? propertyInfo.GetValue(value, null) : null;
-                    }
-                }
-            }
-        );
-
         public ContextTestFixture()
         {
             Context = new Context(new
