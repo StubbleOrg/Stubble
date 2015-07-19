7701b95da3ad99171abea850c8448c624ce40ea1
Romanx <mcauliffe.alex@gmail.com>
2015-07-19 16:08:32 +0100

    StringLoader as default just passing through tmpl

---
+++ Stubble/Classes/StringLoader.cs
@@ -0,0 +1,12 @@
+﻿using Stubble.Core.Interfaces;
+
+namespace Stubble.Core.Classes
+{
+    public class StringLoader : IStubbleLoader
+    {
+        public string Load(string name)
+        {
+            return name;
+        }
+    }
+}
