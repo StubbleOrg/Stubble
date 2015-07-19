37c8defc26675c0760eaf2efc96667dc91f7bbea
Romanx <mcauliffe.alex@gmail.com>
2015-07-19 16:06:25 +0100

    Added IStubbleLoader interface
    This will allow extensions for loading mustache files

---
+++ Stubble/Interfaces/IStubbleLoader.cs
@@ -0,0 +1,12 @@
+﻿namespace Stubble.Core.Interfaces
+{
+    public interface IStubbleLoader
+    {
+        /// <summary>
+        /// Loads a template with the given name
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns>A Mustache Template</returns>
+        string Load(string name);
+    }
+}
