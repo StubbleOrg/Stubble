ac63f9d1fcc6dcb4668baab01459ed8e4f45fe24
Romanx <mcauliffe.alex@gmail.com>
2015-07-22 16:04:51 +0100

    Added DataMissException to throw on data miss
    Only when render settings flag is set

---
+++ Stubble/Classes/Exceptions/StubbleDataMissException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Stubble.Core.Classes.Exceptions
+{
+    public class StubbleDataMissException : Exception
+    {
+        public StubbleDataMissException() { }
+        public StubbleDataMissException(string message) : base(message) { }
+        public StubbleDataMissException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
