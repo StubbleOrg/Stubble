a69351ab733c7810d1d98101916d6c0a448858cc
Romanx <mcauliffe.alex@gmail.com>
2015-07-22 11:22:18 +0100

    Added RenderSettings for exactly as it sounds

---
+++ Stubble/Classes/RenderSettings.cs
@@ -0,0 +1,17 @@
+﻿namespace Stubble.Core.Classes
+{
+    public class RenderSettings
+    {
+        public bool RecursiveLookup { get; set; }
+        public bool ThrowOnDataMiss { get; set; }
+
+        public static RenderSettings GetDefaultRenderSettings()
+        {
+            return new RenderSettings
+            {
+                RecursiveLookup = true,
+                ThrowOnDataMiss = true
+            };
+        }
+    }
+}
