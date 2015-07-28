0cb560b0b4f2f7d750e53117ad5ea757e572410e
Romanx <mcauliffe.alex@gmail.com>
2015-07-28 14:34:08 +0100

    IStubbleBuilder should match StubbleBuilder

--- Stubble/Interfaces/IStubbleBuilder.cs
+++ Stubble/Interfaces/IStubbleBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Stubble.Core.Classes;
 
@@ -10,6 +11,8 @@ namespace Stubble.Core.Interfaces
         IStubbleBuilder AddValueGetter(Type type, Func<object, string, object> valueGetter);
         IStubbleBuilder AddTokenGetter(string tokenType, Func<string, Tags, ParserOutput> tokenGetter);
         IStubbleBuilder AddTokenGetter(KeyValuePair<string, Func<string, Tags, ParserOutput>> tokenGetter);
+        IStubbleBuilder AddEnumerationConversion(KeyValuePair<Type, Func<object, IEnumerable>> enumerationConversion);
+        IStubbleBuilder AddEnumerationConversion(Type type, Func<object, IEnumerable> enumerationConversion);
         IStubbleBuilder AddTruthyCheck(Func<object, bool?> truthyCheck);
         IStubbleBuilder SetTemplateLoader(IStubbleLoader loader);
         IStubbleBuilder SetPartialTemplateLoader(IStubbleLoader loader);
