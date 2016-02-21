c58b2f9ea6acdde1fd4f3222f17e1cf4a6017e81
Romanx <mcauliffe.alex@gmail.com>
2016-02-21 14:24:29 +0000

    Add comments and cleanup Registry class

--- Stubble/Classes/Registry.cs
+++ Stubble/Classes/Registry.cs
@@ -17,117 +17,28 @@ using Stubble.Core.Interfaces;
 
 namespace Stubble.Core.Classes
 {
+    /// <summary>
+    /// A class holding the instance data for a Stubble Renderer
+    /// </summary>
     public sealed class Registry
     {
         private static readonly string[] DefaultTokenTypes = { @"\/", "=", @"\{", "!" };
         private static readonly string[] ReservedTokens = { "name", "text" }; // Name and text are used internally for tokens so must exist
 
-        public IReadOnlyDictionary<Type, Func<object, string, object>> ValueGetters { get; private set; }
-
-        public IReadOnlyDictionary<string, Func<string, Tags, ParserOutput>> TokenGetters { get; private set; }
-
-        public IReadOnlyList<Func<object, bool?>> TruthyChecks { get; private set; }
-
-        public IReadOnlyDictionary<Type, Func<object, IEnumerable>> EnumerationConverters { get; private set; }
-
-        public IStubbleLoader TemplateLoader { get; private set; }
-
-        public IStubbleLoader PartialTemplateLoader { get; private set; }
-
-        public int MaxRecursionDepth { get; private set; }
-
-        public RenderSettings RenderSettings { get; private set; }
-
-        internal Regex TokenMatchRegex { get; private set; }
-
-        #region Default Value Getters
-        private static readonly IDictionary<Type, Func<object, string, object>> DefaultValueGetters = new Dictionary
-            <Type, Func<object, string, object>>
-        {
-            {
-                typeof(IList),
-                (value, key) =>
-                {
-                    var castValue = value as IList;
-
-                    int intVal;
-                    if (int.TryParse(key, out intVal))
-                    {
-                        return castValue != null && intVal < castValue.Count ? castValue[intVal] : null;
-                    }
-
-                    return null;
-                }
-            },
-            {
-                typeof(IDictionary<string, object>),
-                (value, key) =>
-                {
-                    var castValue = value as IDictionary<string, object>;
-                    return castValue != null && castValue.ContainsKey(key) ? castValue[key] : null;
-                }
-            },
-            {
-                typeof(IDictionary),
-                (value, key) =>
-                {
-                    var castValue = value as IDictionary;
-                    return castValue != null ? castValue[key] : null;
-                }
-            },
-            {
-                typeof(object), (value, key) =>
-                {
-                    var type = value.GetType();
-                    var memberArr = type.GetMember(key, BindingFlags.Public | BindingFlags.Static | BindingFlags.Instance | BindingFlags.FlattenHierarchy);
-                    if (memberArr.Length != 1)
-                    {
-                        return null;
-                    }
-
-                    var member = memberArr[0];
-                    switch (member.MemberType)
-                    {
-                        case MemberTypes.Field:
-                            return ((FieldInfo)member).GetValue(value);
-                        case MemberTypes.Property:
-                            return ((PropertyInfo)member).GetValue(value, null);
-                        case MemberTypes.Method:
-                            var methodMember = (MethodInfo)member;
-                            return methodMember.GetParameters().Length == 0
-                                ? methodMember.Invoke(value, null)
-                                : null;
-                        default:
-                            return null;
-                    }
-                }
-            }
-        };
-
-        #endregion
-        #region Default Token Getters
-        private static readonly IDictionary<string, Func<string, Tags, ParserOutput>> DefaultTokenGetters = new Dictionary
-            <string, Func<string, Tags, ParserOutput>>
-        {
-            { "#", (s, tags) => new SectionToken() { TokenType = s, Tags = tags } },
-            { "^", (s, tags) => new InvertedToken() { TokenType = s } },
-            { ">", (s, tags) => new PartialToken() { TokenType = s } },
-            { "&", (s, tags) => new UnescapedValueToken() { TokenType = s } },
-            { "name", (s, tags) => new EscapedValueToken() { TokenType = s } },
-            { "text", (s, tags) => new RawValueToken() { TokenType = s } }
-        };
-        #endregion
-        #region Default Enumeration Converters
-
-        private static readonly IDictionary<Type, Func<object, IEnumerable>> DefaultEnumerationConverters = new Dictionary
-            <Type, Func<object, IEnumerable>>();
-        #endregion
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Registry"/> class
+        /// with default <see cref="RegistrySettings"/>
+        /// </summary>
         public Registry()
             : this(default(RegistrySettings))
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Registry"/> class
+        /// with given <see cref="RegistrySettings"/>
+        /// </summary>
+        /// <param name="settings">The registry settings to initalise the Registry with</param>
         public Registry(RegistrySettings settings)
         {
             SetValueGetters(settings.ValueGetters);
@@ -141,11 +52,56 @@ namespace Stubble.Core.Classes
             RenderSettings = settings.RenderSettings ?? RenderSettings.GetDefaultRenderSettings();
         }
 
+        /// <summary>
+        /// Gets a readonly dictionary of Value Getter functions
+        /// </summary>
+        public IReadOnlyDictionary<Type, Func<object, string, object>> ValueGetters { get; private set; }
+
+        /// <summary>
+        /// Gets a readonly dictionary of Token Getter functions
+        /// </summary>
+        public IReadOnlyDictionary<string, Func<string, Tags, ParserOutput>> TokenGetters { get; private set; }
+
+        /// <summary>
+        /// Gets a readonly list of Truthy Checks
+        /// </summary>
+        public IReadOnlyList<Func<object, bool?>> TruthyChecks { get; private set; }
+
+        /// <summary>
+        /// Gets a readonly dictionary of EnumerationConverters
+        /// </summary>
+        public IReadOnlyDictionary<Type, Func<object, IEnumerable>> EnumerationConverters { get; private set; }
+
+        /// <summary>
+        /// Gets the template loader for the Stubble instance
+        /// </summary>
+        public IStubbleLoader TemplateLoader { get; private set; }
+
+        /// <summary>
+        /// Gets the partial template loader for the Stubble instance
+        /// </summary>
+        public IStubbleLoader PartialTemplateLoader { get; private set; }
+
+        /// <summary>
+        /// Gets the max recursion depth for the render call
+        /// </summary>
+        public int MaxRecursionDepth { get; private set; }
+
+        /// <summary>
+        /// Gets the <see cref="RenderSettings"/> for the Stubble instance
+        /// </summary>
+        public RenderSettings RenderSettings { get; private set; }
+
+        /// <summary>
+        /// Gets the generated Token match regex
+        /// </summary>
+        internal Regex TokenMatchRegex { get; private set; }
+
         private void SetValueGetters(IDictionary<Type, Func<object, string, object>> valueGetters)
         {
             if (valueGetters != null)
             {
-                var mergedGetters = DefaultValueGetters.MergeLeft(valueGetters);
+                var mergedGetters = RegistryDefaults.DefaultValueGetters.MergeLeft(valueGetters);
 
                 mergedGetters = mergedGetters
                     .OrderBy(x => x.Key, TypeBySubclassAndAssignableImpl.TypeBySubclassAndAssignable())
@@ -155,7 +111,7 @@ namespace Stubble.Core.Classes
             }
             else
             {
-                ValueGetters = new ReadOnlyDictionary<Type, Func<object, string, object>>(DefaultValueGetters);
+                ValueGetters = new ReadOnlyDictionary<Type, Func<object, string, object>>(RegistryDefaults.DefaultValueGetters);
             }
         }
 
@@ -163,13 +119,13 @@ namespace Stubble.Core.Classes
         {
             if (tokenGetters != null)
             {
-                var mergedGetters = DefaultTokenGetters.MergeLeft(tokenGetters);
+                var mergedGetters = RegistryDefaults.DefaultTokenGetters.MergeLeft(tokenGetters);
 
                 TokenGetters = new ReadOnlyDictionary<string, Func<string, Tags, ParserOutput>>(mergedGetters);
             }
             else
             {
-                TokenGetters = new ReadOnlyDictionary<string, Func<string, Tags, ParserOutput>>(DefaultTokenGetters);
+                TokenGetters = new ReadOnlyDictionary<string, Func<string, Tags, ParserOutput>>(RegistryDefaults.DefaultTokenGetters);
             }
         }
 
@@ -182,12 +138,12 @@ namespace Stubble.Core.Classes
         {
             if (enumerationConverters != null)
             {
-                var mergedGetters = DefaultEnumerationConverters.MergeLeft(enumerationConverters);
+                var mergedGetters = RegistryDefaults.DefaultEnumerationConverters.MergeLeft(enumerationConverters);
                 EnumerationConverters = new ReadOnlyDictionary<Type, Func<object, IEnumerable>>(mergedGetters);
             }
             else
             {
-                EnumerationConverters = new ReadOnlyDictionary<Type, Func<object, IEnumerable>>(DefaultEnumerationConverters);
+                EnumerationConverters = new ReadOnlyDictionary<Type, Func<object, IEnumerable>>(RegistryDefaults.DefaultEnumerationConverters);
             }
         }
 
@@ -207,6 +163,85 @@ namespace Stubble.Core.Classes
                 string.Join("|", TokenGetters.Where(s => !ReservedTokens.Contains(s.Key))
                                         .Select(s => Parser.EscapeRegexExpression(s.Key))
                                         .Concat(DefaultTokenTypes)));
+        }
+
+        private static class RegistryDefaults
+        {
+            public static readonly IDictionary<Type, Func<object, string, object>> DefaultValueGetters = new Dictionary <Type, Func<object, string, object>>
+            {
+                {
+                    typeof(IList),
+                    (value, key) =>
+                    {
+                        var castValue = value as IList;
+
+                        int intVal;
+                        if (int.TryParse(key, out intVal))
+                        {
+                            return castValue != null && intVal < castValue.Count ? castValue[intVal] : null;
+                        }
+
+                        return null;
+                    }
+                },
+                {
+                    typeof(IDictionary<string, object>),
+                    (value, key) =>
+                    {
+                        var castValue = value as IDictionary<string, object>;
+                        return castValue != null && castValue.ContainsKey(key) ? castValue[key] : null;
+                    }
+                },
+                {
+                    typeof(IDictionary),
+                    (value, key) =>
+                    {
+                        var castValue = value as IDictionary;
+                        return castValue != null ? castValue[key] : null;
+                    }
+                },
+                {
+                    typeof(object), (value, key) =>
+                    {
+                        var type = value.GetType();
+                        var memberArr = type.GetMember(key, BindingFlags.Public | BindingFlags.Static | BindingFlags.Instance | BindingFlags.FlattenHierarchy);
+                        if (memberArr.Length != 1)
+                        {
+                            return null;
+                        }
+
+                        var member = memberArr[0];
+                        switch (member.MemberType)
+                        {
+                            case MemberTypes.Field:
+                                return ((FieldInfo)member).GetValue(value);
+                            case MemberTypes.Property:
+                                return ((PropertyInfo)member).GetValue(value, null);
+                            case MemberTypes.Method:
+                                var methodMember = (MethodInfo)member;
+                                return methodMember.GetParameters().Length == 0
+                                    ? methodMember.Invoke(value, null)
+                                    : null;
+                            default:
+                                return null;
+                        }
+                    }
+                }
+            };
+
+            public static readonly IDictionary<string, Func<string, Tags, ParserOutput>> DefaultTokenGetters = new Dictionary
+                <string, Func<string, Tags, ParserOutput>>
+            {
+                { "#", (s, tags) => new SectionToken() { TokenType = s, Tags = tags } },
+                { "^", (s, tags) => new InvertedToken() { TokenType = s } },
+                { ">", (s, tags) => new PartialToken() { TokenType = s } },
+                { "&", (s, tags) => new UnescapedValueToken() { TokenType = s } },
+                { "name", (s, tags) => new EscapedValueToken() { TokenType = s } },
+                { "text", (s, tags) => new RawValueToken() { TokenType = s } }
+            };
+
+            public static readonly IDictionary<Type, Func<object, IEnumerable>> DefaultEnumerationConverters = new Dictionary
+                <Type, Func<object, IEnumerable>>();
         }
     }
 }
