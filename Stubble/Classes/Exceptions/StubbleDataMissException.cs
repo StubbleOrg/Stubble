b72b3245335c739604317d32f9ce63acfa94a08f
Romanx <mcauliffe.alex@gmail.com>
2016-02-21 14:23:52 +0000

    Update all Exceptions to extend from StubbleException

--- Stubble/Classes/Exceptions/StubbleDataMissException.cs
+++ Stubble/Classes/Exceptions/StubbleDataMissException.cs
@@ -7,17 +7,35 @@ using System;
 
 namespace Stubble.Core.Classes.Exceptions
 {
-    public class StubbleDataMissException : Exception
+    /// <summary>
+    /// Represents errors that occur when data is not found when rendering a template
+    /// </summary>
+    public class StubbleDataMissException : StubbleException
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StubbleDataMissException"/> class.
+        /// </summary>
         public StubbleDataMissException()
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StubbleDataMissException"/> class with
+        /// a specified error message.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
         public StubbleDataMissException(string message)
             : base(message)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StubbleDataMissException"/> class with
+        /// a specified error message and a reference to the inner exception that is the cause of
+        /// this exception.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception, or a null reference if no inner exception is specified.</param>
         public StubbleDataMissException(string message, Exception innerException)
             : base(message, innerException)
         {
--- Stubble/Classes/Exceptions/StubbleException.cs
+++ Stubble/Classes/Exceptions/StubbleException.cs
@@ -7,17 +7,35 @@ using System;
 
 namespace Stubble.Core.Classes.Exceptions
 {
+    /// <summary>
+    /// Represents a general error coming from Stubble
+    /// </summary>
     public class StubbleException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StubbleException"/> class.
+        /// </summary>
         public StubbleException()
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StubbleException"/> class with
+        /// a specified error message.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
         public StubbleException(string message)
             : base(message)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StubbleException"/> class with
+        /// a specified error message and a reference to the inner exception that is the cause of
+        /// this exception.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception, or a null reference if no inner exception is specified.</param>
         public StubbleException(string message, Exception innerException)
             : base(message, innerException)
         {
