bf8384b52e9663556663e77e8e5829895cb0501a
Romanx <mcauliffe.alex@gmail.com>
2017-05-07 16:33:33 +0100

    Add LoadAsync to IStubbleLoader #15

--- src/Stubble.Core/Loaders/StringLoader.cs
+++ src/Stubble.Core/Loaders/StringLoader.cs
@@ -3,6 +3,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
 
+using System.Threading.Tasks;
 using Stubble.Core.Interfaces;
 
 namespace Stubble.Core.Loaders
@@ -21,5 +22,12 @@ namespace Stubble.Core.Loaders
         /// <param name="name">The name of the template to load</param>
         /// <returns>A Mustache Template</returns>
         public string Load(string name) => name;
+
+        /// <summary>
+        /// Returns the passed string as the template
+        /// </summary>
+        /// <param name="name">The template</param>
+        /// <returns>The template as a task</returns>
+        public ValueTask<string> LoadAsync(string name) => new ValueTask<string>(name);
     }
 }
