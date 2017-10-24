20ae19e7acfd6a21360bd029a80dd81f820bb2e9
Romanx <mcauliffe.alex@gmail.com>
2017-10-24 18:10:21 +0100

    Remove strongnaming
    Users can replace it with tools such as Strongnamer

--- src/Stubble.Core/Properties/AssemblyInfo.cs
+++ src/Stubble.Core/Properties/AssemblyInfo.cs
@@ -10,12 +10,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: InternalsVisibleTo("Stubble.Core.Tests,PublicKey=" +
-"00240000048000009400000006020000002400005253413100040000010001004704c42e715bb3" +
-"70ce2b5662560a34210a9bb2f25ca48687d7a35879a2a96472c80edb71fe8b4cc05cf1f5a0397c" +
-"5e4dbd40022d41c5e2f73780bd398adde68fe355148b9d370376eb908ccb5e7db4383b1dedc039" +
-"b58d8f7066e6b7ca8d5bf8b26f326f5e589ce1658ae9e9b581b483a7989fb6f3ecd37a9132c334" +
-"8c90b8b2")]
+[assembly: InternalsVisibleTo("Stubble.Core.Tests")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
