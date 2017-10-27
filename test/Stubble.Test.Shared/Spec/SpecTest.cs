ae35188f661d0344e838f52ef7ab7d5cd1f2a26b
Romanx <mcauliffe.alex@gmail.com>
2017-10-27 20:32:21 +0100

    Add serialization to spec test class
    Makes VS show the test cases individually for targeting

--- test/Stubble.Test.Shared/Spec/SpecTest.cs
+++ test/Stubble.Test.Shared/Spec/SpecTest.cs
@@ -3,11 +3,13 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
 
+using Newtonsoft.Json;
 using System.Collections.Generic;
+using Xunit.Abstractions;
 
 namespace Stubble.Test.Shared.Spec
 {
-    public class SpecTest
+    public class SpecTest : IXunitSerializable
     {
         public string Name { get; set; }
 
@@ -22,5 +24,32 @@ namespace Stubble.Test.Shared.Spec
         public bool Skip { get; set; }
 
         public IDictionary<string, string> Partials { get; set; }
+
+        private static JsonSerializerSettings serializerSettings = new JsonSerializerSettings
+        {
+            TypeNameHandling = TypeNameHandling.All
+        };
+
+        public void Deserialize(IXunitSerializationInfo info)
+        {
+            Name = info.GetValue<string>(nameof(Name));
+            Desc = info.GetValue<string>(nameof(Desc));
+            Data = JsonConvert.DeserializeObject(info.GetValue<string>(nameof(Data)), serializerSettings);
+            Template = info.GetValue<string>(nameof(Template));
+            Expected = info.GetValue<string>(nameof(Expected));
+            Partials = JsonConvert.DeserializeObject<Dictionary<string, string>>(info.GetValue<string>(nameof(Partials)));
+            Skip = info.GetValue<bool>(nameof(Skip));
+        }
+
+        public void Serialize(IXunitSerializationInfo info)
+        {
+            info.AddValue(nameof(Name), Name);
+            info.AddValue(nameof(Desc), Desc);
+            info.AddValue(nameof(Data), JsonConvert.SerializeObject(Data, serializerSettings));
+            info.AddValue(nameof(Partials), JsonConvert.SerializeObject(Partials, serializerSettings));
+            info.AddValue(nameof(Template), Template);
+            info.AddValue(nameof(Expected), Expected);
+            info.AddValue(nameof(Skip), Skip);
+        }
     }
 }
