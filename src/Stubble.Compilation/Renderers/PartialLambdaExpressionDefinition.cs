73f9c0bbecff0264e59bc6f5a80174840e4d101d
Romanx <mcauliffe.alex@gmail.com>
2017-11-08 15:01:26 +0000

    Adds key function for partial lambdas
    Allows including type information in method call

--- src/Stubble.Compilation/Renderers/PartialLambdaExpressionDefinition.cs
+++ src/Stubble.Compilation/Renderers/PartialLambdaExpressionDefinition.cs
@@ -3,6 +3,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
 
+using System;
 using System.Linq.Expressions;
 
 namespace Stubble.Compilation.Renderers
@@ -21,5 +22,13 @@ namespace Stubble.Compilation.Renderers
         /// Gets or sets the lambda expression representing the partial
         /// </summary>
         public LambdaExpression Partial { get; set; }
+
+        /// <summary>
+        /// Gets the key that will be used to identify the partial lambda
+        /// </summary>
+        /// <param name="template">The template the partial represents</param>
+        /// <param name="type">The type used as an argument for the partial</param>
+        /// <returns>The key</returns>
+        public static string GetKey(string template, Type type) => template + " " + type.FullName;
     }
 }
