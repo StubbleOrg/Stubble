f07ac62aff468db0743a6363491c703919020ece
Romanx <mcauliffe.alex@gmail.com>
2018-09-30 14:22:21 +0100

    Add customization to CompilerSettings
    Allows value getters and Enumeration converters
    
    #32

---
+++ src/Stubble.Compilation/Class/EnumerationConverter.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="EnumerationConverter.cs" company="Stubble Authors">
+// Copyright (c) Stubble Authors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using System;
+using System.Collections;
+using System.Linq.Expressions;
+
+namespace Stubble.Compilation.Class
+{
+    /// <summary>
+    /// A class for holding the conversion to an IEnumerable with its inner type
+    /// </summary>
+    public class EnumerationConverter
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EnumerationConverter"/> class.
+        /// </summary>
+        /// <param name="converterExpression">An expression for converting to IEnumerable</param>
+        /// <param name="childType">The inner type of the IEnumerable</param>
+        public EnumerationConverter(
+            Expression<Func<object, IEnumerable>> converterExpression,
+            Type childType)
+        {
+            ConverterExpression = converterExpression;
+            ChildType = childType;
+        }
+
+        /// <summary>
+        /// Gets the converter to IEnumerable
+        /// </summary>
+        public Expression<Func<object, IEnumerable>> ConverterExpression { get; }
+
+        /// <summary>
+        /// Gets the child type to convert to
+        /// </summary>
+        public Type ChildType { get; }
+    }
+}
--- test/Stubble.Compilation.Tests/ContextTests.cs
+++ test/Stubble.Compilation.Tests/ContextTests.cs
@@ -1,10 +1,9 @@
-﻿using Stubble.Compilation.Settings;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
+using System.Data;
+using System.Linq.Expressions;
+using Stubble.Compilation.Settings;
 using Xunit;
+using System.Linq;
 
 namespace Stubble.Compilation.Tests
 {
@@ -89,6 +88,94 @@ namespace Stubble.Compilation.Tests
                 Foo = (string)null,
                 Bar = "Display Me"
             }));
+        }
+
+        [Fact]
+        public void ComplicationRenderer_ItShouldAllowCustomValueGetters()
+        {
+            var builder = new CompilerSettingsBuilder();
+
+            builder.AddValueGetter<DifficultClass>((type, instance, key, ignoreCase) =>
+            {
+                return Expression.Call(
+                    instance,
+                    typeof(DifficultClass).GetMethod("GetValue"),
+                    Expression.Constant(key),
+                    Expression.Constant(ignoreCase ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal));
+            });
+
+            var stubble = new StubbleCompilationRenderer(builder.BuildSettings());
+
+            var obj = new DifficultClass("Foo", "Bar");
+
+            var func = stubble.Compile("{{_innerVal}} {{_innerVal2}}", obj);
+
+            Assert.Equal("Foo Bar", func(obj));
+        }
+
+        [Fact]
+        public void ComplicationRenderer_ItShouldAllowCustomEnumerationConverters()
+        {
+            var builder = new CompilerSettingsBuilder();
+
+            builder.AddValueGetter<DataRow>((type, instance, key, ignoreCase) =>
+            {
+                var contains = Expression.Call(Expression.Property(
+                    Expression.Property(instance, nameof(DataRow.Table)),
+                    nameof(DataTable.Columns)),
+                    typeof(DataColumnCollection).GetMethod(nameof(DataColumnCollection.Contains)),
+                    Expression.Constant(key));
+
+                return Expression.Condition(contains,
+                   Expression.Property(instance, "Item", Expression.Constant(key)),
+                   Expression.Constant(null)
+                );
+            });
+            builder.AddEnumerationConverter((DataTable dt) => dt.Rows.Cast<DataRow>());
+
+            var stubble = new StubbleCompilationRenderer(builder.BuildSettings());
+
+            var obj = new DataTable();
+            obj.Columns.Add(new DataColumn("Column1"));
+            obj.Columns.Add(new DataColumn("Column2"));
+            obj.Columns.Add(new DataColumn("Column3"));
+
+            obj.Rows.Add(1, "foo", "bar");
+            obj.Rows.Add(2, "foo", "bar");
+            obj.Rows.Add(3, "foo", "bar");
+
+            var func = stubble.Compile("{{#dt}}{{Column1}} {{Column2}}.\n{{/dt}}", new { dt = obj });
+
+            Assert.Equal(@"1 foo.
+2 foo.
+3 foo.
+", func(new { dt = obj }));
+        }
+
+        private class DifficultClass
+        {
+            private readonly string _innerVal;
+            private readonly string _innerVal2;
+
+            public DifficultClass(string val, string val2)
+            {
+                _innerVal = val;
+                _innerVal2 = val2;
+            }
+
+            public object GetValue(string value, StringComparison comparison)
+            {
+                if (value.Equals(nameof(_innerVal), comparison))
+                {
+                    return _innerVal;
+                }
+                if (value.Equals(nameof(_innerVal2), comparison))
+                {
+                    return _innerVal2;
+                }
+
+                return null;
+            }
         }
     }
 }
